974c68853c99d42e2ea544cc20b2e7a0fa441330
Andrew Apollonsky <Anapollonsky@gmail.com>
2014-12-31 16:30:22 -0500

    Switched to urxvt d/c, added layouts to xmonad.

--- xmonad.hs
+++ xmonad.hs
@@ -1,13 +1,21 @@
 -- Andrew Apollonsky xmonad configuration
 -- Source for most of this:
 -- https://www.haskell.org/haskellwiki/Xmonad/Config_archive/John_Goerzen's_Configuration
-
+import qualified Data.Map as M
 import XMonad
 import XMonad.Actions.Navigation2D
+import XMonad.Actions.UpdatePointer
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Layout.MultiToggle
+import XMonad.Layout.MultiToggle.Instances
+import XMonad.Layout.Renamed
+import XMonad.Layout.Grid
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.Spiral
+import XMonad.Layout.Tabbed
+import XMonad.Layout.Reflect
+import XMonad.Util.Run
 import System.IO
 
 main = do
@@ -15,29 +23,50 @@ main = do
     xmonad $ defaultConfig
         {
 -------------------- basics
-          terminal = "urxvt"
+          terminal = "urxvtc"
         , modMask = mod4Mask -- windows as mod key
         , focusedBorderColor = "#AAAAFF"
-        , normalBorderColor = "#7777AA"
-
+        , normalBorderColor = "#555599"
+        , borderWidth = 1
 -------------------- xmobar                               
         , manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+--        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , layoutHook = myLayouts
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "#A0E030" "" . shorten 50
+                        , ppHiddenNoWindows = xmobarColor "grey" ""
                         }
-        } `additionalKeys`
--------------------- keys        
-        [ ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock") -- super+shift+l = lock
-        , ((mod4Mask, xK_Print), spawn "sleep 0.2; scrot -s") -- super + printscreen = screenshot of window
+-------------------- other
+        , keys = \c -> myKeys c `M.union` keys defaultConfig c
+        }
+
+-------------------- layouts
+myLayouts = avoidStruts
+            $ mkToggle (REFLECTX ?? REFLECTY ?? MIRROR ?? FULL ?? EOT)
+            $ Tall 1 (3/1000) (1/2)
+--            ||| ThreeCol 1 (3/100) (1/2)
+            ||| renamed [Replace "ThreeColMid"] (ThreeColMid 1 (3/100) (1/2))
+            ||| Grid
+            ||| spiral (6/7)
+
+-------------------- keybindings
+myKeys conf@(XConfig {XMonad.modMask = modm}) =
+  M.fromList
+  $     [ ((modm .|. shiftMask, xK_l), spawn "xscreensaver-command -lock") -- super+shift+l = lock
+        , ((modm, xK_Print), spawn "sleep 0.2; scrot -s") -- super + printscreen = screenshot of window
         , ((0, xK_Print), spawn "scrot") -- printscreen = screenshot of everything. screenshot reqs "scrot"
-        , ((mod4Mask, xK_a), spawn "emacs")
-        , ((mod4Mask, xK_s), spawn "conkeror")
-        , ((mod4Mask, xK_d), spawn "dmenu_run")          
+        , ((modm, xK_a), spawn "emacs")
+        , ((modm, xK_s), spawn "conkeror")
+        , ((modm, xK_d), spawn "dmenu_run")
           -- direction navigation of windows
-        , ((mod4Mask,                 xK_Right), windowGo R True)
-        , ((mod4Mask,                 xK_Left ), windowGo L True)
-        , ((mod4Mask,                 xK_Up   ), windowGo U True)
-        , ((mod4Mask,                 xK_Down ), windowGo D True)          
+        , ((modm,                 xK_Right), windowGo R True)
+        , ((modm,                 xK_Left ), windowGo L True)
+        , ((modm,                 xK_Up   ), windowGo U True)
+        , ((modm,                 xK_Down ), windowGo D True)
+        , ((modm,               xK_m), sendMessage $ Toggle MIRROR)
+        --, ((modm,               xK_f), sendMessage $ Toggle TABBED)
+        , ((modm,               xK_f), sendMessage $ Toggle FULL)
+        , ((modm,               xK_x), sendMessage $ Toggle REFLECTX)
+        , ((modm,               xK_y), sendMessage $ Toggle REFLECTY)
         ]
