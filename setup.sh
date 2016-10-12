aecd637b25d8f77a0f27eb60cba64fe354ffadff
Andrew Apollonsky <andrew.apollonsky@yodle.com>
2016-10-12 15:02:05 -0500

    Added ideavimrc nicer

--- setup.sh
+++ setup.sh
@@ -34,6 +34,7 @@ file_dest_map=($SCRIPT_DIR/zshrc $HOME_DIR/.zshrc
                $SCRIPT_DIR/rofi_dmenu /usr/bin/rofi_dmenu # Messy
                $SCRIPT_DIR/taffybar.hs $HOME_DIR/.config/taffybar/taffybar.hs
                $SCRIPT_DIR/taffybar.rc $HOME_DIR/.config/taffybar/taffybar.rc
+               $SCRIPT_DIR/ideavimrc $HOME_DIR/.ideavimrc
                $SCRIPT_DIR/gitignore $HOME_DIR/.config/git/ignore
               )
 
